10298ccc5d0e70db33948543f3427c1e7089dead
Gabriel Baduqui <baduquig@gmail.com>
2025-04-05 13:13:01 -0700

    login.tsx WIP...

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+
+const hostURL = 'http://127.0.0.1:5000/login';
+
+interface UserLogin {
+    username: string;
+    password: string;
+}
+
 const LoginInputs = () => {
+    const [usernamePopulated, setUsernamePopulated] = useState(false);
+    const [passwordPopulated, setPasswordPopulated] = useState(false);
+
+    const loginRequest = async (user: UserLogin) => {
+        const response = await fetch(hostURL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({username: user.username, password: user.password})
+        });
+        
+        if (!response.ok) {
+            console.log(`Request error! ${response.status}`);
+        }
+        const loginResponse = await response.json()
+        return loginResponse;
+    };
+
     return (
         <div>
 
@@ -21,6 +47,7 @@ const LoginInputs = () => {
             <button
                 className="submitButton"
                 id="loginButton"
+                //onClick={loginRequest({usernameInputValue, passwordInputValue})}
                 type="submit"
             />
             

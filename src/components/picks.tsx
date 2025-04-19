89430966a0082353c57c44869e2d6a197b957dbc
Gabriel Baduqui <baduquig@gmail.com>
2025-04-18 22:42:15 -0700

    Picks component WIP...

--- src/components/picks.tsx
+++ src/components/picks.tsx
@@ -2,21 +2,55 @@ import { useEffect, useState } from "react";
 
 const hostURL: string = "http://127.0.0.1:5000/picks/gbtest3";
 
-const FetchUserPicks = async () => {
-    const response = await fetch(hostURL);
-    
-    if (!response.ok) {
-        console.log(`Request error! ${response.status}`);
-    } else {
-        console.log(response.json())
-    }
-    
-};
+interface Pick {
+    gameID: string;
+    teamPicked: string;
+    pickWeight: string;
+    username: string
+}
+
+type PicksArray = Array<Pick>;
+
+const PickRow = (props: Pick) => {
+    return (
+        <p>{props.gameID} | {props.teamPicked} | {props.pickWeight}</p>
+    )
+}
+
 
 const PicksContainer = () => {
-    useEffect(() => { FetchUserPicks() })
+    const [allUserPicks, setAllUserPicks] = useState();
+    const [week, setWeek] = useState(1);
+
+    useEffect(() => {
+        const fetchUserPicks = async () => {
+            try {
+                const response = await fetch(hostURL);
+                if (!response.ok) {
+                    setAllUserPicks();
+                    console.log(`Request error! ${response.status}`);
+                } else {
+                    const data = await response.json();
+                    setAllUserPicks(data);
+                    console.log(data);
+                }
+            } catch (err) {
+                console.log(`Request failed! ${err}`)
+            }
+        }
+
+        fetchUserPicks();
+    }, []);
+
     return (
-        <div>Hello</div>
+        allUserPicks.map(pick => {
+            <PickRow
+                gameID={pick.gameID}
+                teamPicked={pick.teamPicked}
+                pickWeight={pick.pickWeight}
+                username={pick.username}
+            />
+        })
     )
 }
 

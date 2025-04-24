ff1a7f3359fcfceae8d721d1eb02823642e69f43
Gabriel Baduqui <baduquig@gmail.com>
2025-04-23 22:36:41 -0700

    Picks component WIP...

--- src/components/picks.tsx
+++ src/components/picks.tsx
@@ -4,18 +4,68 @@ import { Game } from "./game";
 import { Team } from "./team";
 import { Pick } from "./pick";
 
+interface teamOptionProps {
+    props: {
+        teams: Array<Team>;
+        teamID: string;
+    }
+}
+
+const awayTeamOption = ({ props }: teamOptionProps) => {
+    const team: Team = props.teams.find(t => t.teamID === props.teamID);
+    const infoCellID: string = `info-${team.teamID}`;
+    
+    return (
+        <td>
+            <span className="infoCell" id={infoCellID}>
+                i
+            </span>
+            <img src={team.teamLogoUrl} alt={team.teamName} />
+        </td>
+    )
+}
+
+const homeTeamOption = ({ props }: teamOptionProps) => {
+    const team: Team = props.teams.find(t => t.teamID === props.teamID);
+    const infoCellID: string = `info-${team.teamID}`;
+
+    return (
+        <td>
+            <img src={team.teamLogoUrl} alt={team.teamName} />
+            <span className="infoCell" id={infoCellID}>
+                i
+            </span>
+        </td>
+    )
+}
+
+const pickRow = (game: Game) => {
+    const infoCellID: string = `info-${game.gameID}`;
+
+    return (
+        <tr>
+            <awayTeamOption props={ teams } />
+        </tr>
+    )
+}
 
 
 const PicksContainer = () => {
-    const [games, setGames] = useState([]);
-    const [teams, setTeams] = useState([]);
-    const [allUserPicks, setAllUserPicks] = useState([]);
+    const [games, setGames] = useState(Array<Game>);
+    const [teams, setTeams] = useState(Array<Team>);
+    const [allUserPicks, setAllUserPicks] = useState(Array<Pick>);
     const [week, setWeek] = useState(1);
+    const [weekGames, setWeekGames] = useState(Array<Game>)
+
+    const filterWeek = games.filter(game => {
+        game.week === week;
+    });
 
     useEffect(() => {
         getGames();
         getTeams();
         getUserPicks(38);
+        setWeekGames(filterWeek);
     }, [setGames, setTeams, setAllUserPicks]);
 
     return (

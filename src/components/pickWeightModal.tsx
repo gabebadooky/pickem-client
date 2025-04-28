f45788152e6828f1c7d7403fb608fdaee16feffc
Gabriel Baduqui <baduquig@gmail.com>
2025-04-27 22:21:15 -0700

    pickWeightModal WIP...

---
+++ src/components/pickWeightModal.tsx
@@ -0,0 +1,9 @@
+export default function PickWeight({ onClose }) {
+    return (
+      <div className="modal">
+        <div>I'm a modal dialog</div>
+        <button onClick={onClose}>Close</button>
+      </div>
+    );
+  }
+  

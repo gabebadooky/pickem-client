015c25d0a555751ebd09e88a95d8f0c0f638b537
gbaduqui <baduquig@gmail.com>
2025-07-17 12:55:13 -0700

    Login flow isLoading functionality

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
     return(
-        <div>
+        <div className="fixed flex h-[100vh] items-center justify-center left-0 top-0 w-[100vw] z-1000">
             <img src="../assets/loadingSpinner.gif" alt="" />
         </div>
     )
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,10 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
+import { createPortal } from "react-dom";
 import { loginRequest } from "../services/authAPI";
 import { LoginBody, NullLoginBody } from "../types/user";
 import { Link, useNavigate } from "react-router";
 import { Team } from "../types/team";
+import LoadingSpinner from "./LoadingSpinner";
 
 
 type Props = {
@@ -16,6 +18,7 @@ const Login = (props: Props) => {
     const [passwordPopulated, setPasswordPopulated] = useState<boolean>(false);
     const [loginBody, setLoginBody] = useState<LoginBody>(NullLoginBody);
     const [warningMessageVisible, setWarningMessageVisible] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const navigate = useNavigate();
     
 
@@ -49,12 +52,10 @@ const Login = (props: Props) => {
 
     const attemptLogin = (e: FormEvent) => {
         e.preventDefault;
-
+        setIsLoading(true);
         loginRequest(loginBody)
         .then((response) => {
             if (response?.access_token) {
-                console.log(`response.access_token: ${response.access_token}`);
-                console.log(`response["access_token"]: ${response["access_token"]}`);
                 localStorage.setItem("jwt", response["access_token"]);
                 props.setIsAuthenticated(true);
                 navigate("/");
@@ -62,6 +63,7 @@ const Login = (props: Props) => {
             } else {
                 setWarningMessageVisible(true);
             }
+            setIsLoading(false);
         })
         .catch((err) => {
             console.log(err);
@@ -72,7 +74,15 @@ const Login = (props: Props) => {
 
     return (
         <div>
-            <h1 className="mt-35 mb-7 text-xl">Pickem</h1>
+
+            {
+                isLoading
+                    &&
+                (createPortal(
+                    <LoadingSpinner />,
+                    document.body
+                ))
+            }
 
             {
                 warningMessageVisible
@@ -82,48 +92,57 @@ const Login = (props: Props) => {
                 </p>
             }
 
-            <input
-                autoComplete="username"
-                className="bg-[#D9D9D9] h-6 mb-3 rounded-xl text-black text-center w-[90%]"
-                id="usernameInput"
-                onChange={handleTextInputChange}
-                placeholder="Username"
-                type="text"
-                value={loginBody.username}
-            />
-
-            <br />
-
-            <input
-                autoComplete="current-password"
-                className="bg-[#D9D9D9] h-6 mb-7 rounded-xl text-black text-center w-[90%]"
-                id="passwordInput"
-                onChange={handleTextInputChange}
-                placeholder="Password"
-                type="password"
-                value={loginBody.password}
-            />
-
-            <br />
-            
             {
-                usernamePopulated && passwordPopulated
+                !isLoading
                     &&
-                <button className="bg-[#17C120] h-6 mt-2 mb-2 rounded-xl w-[90%]" id="loginButton" type="submit"
-                        onClick={(e) => attemptLogin(e)}
-                >
-                    Login
-                </button>
+                <>
+
+                    <h1 className="mt-35 mb-7 text-xl">Pickem</h1>
+                    
+                    <input
+                        autoComplete="username"
+                        className="bg-[#D9D9D9] h-6 mb-3 rounded-xl text-black text-center w-[90%]"
+                        id="usernameInput"
+                        onChange={handleTextInputChange}
+                        placeholder="Username"
+                        type="text"
+                        value={loginBody.username}
+                    />
+
+                    <br />
+
+                    <input
+                        autoComplete="current-password"
+                        className="bg-[#D9D9D9] h-6 mb-7 rounded-xl text-black text-center w-[90%]"
+                        id="passwordInput"
+                        onChange={handleTextInputChange}
+                        placeholder="Password"
+                        type="password"
+                        value={loginBody.password}
+                    />
+
+                    <br />
+                    
+                    {
+                        usernamePopulated && passwordPopulated
+                            &&
+                        <button className="bg-[#17C120] h-6 mt-2 mb-2 rounded-xl w-[90%]" id="loginButton" type="submit"
+                                onClick={(e) => attemptLogin(e)}
+                        >
+                            Login
+                        </button>
+                    }
+
+                    <br />
+
+                    <Link to="/register" state={{teams: props.teams}}>
+                        <button className="border-1 border-white mb-5 px-2 py-1 rounded-lg w-[90%]">
+                            Create Account
+                        </button>
+                    </Link>
+
+                </>
             }
-
-            <br />
-
-            <Link to="/register" state={{teams: props.teams}}>
-            <button className="border-1 border-white mb-5 px-2 py-1 rounded-lg w-[90%]">
-                Create Account
-            </button>
-            </Link>
-
         </div>
     )
 }

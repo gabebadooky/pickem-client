5127b952edd4d1087e5a56a37da3d3fa8d1e476d
Gabriel Baduqui <baduquig@gmail.com>
2025-07-20 21:26:09 -0700

    reformat GameInfoModal component

--- src/components/GameInfoModal.tsx
+++ src/components/GameInfoModal.tsx
@@ -1,6 +1,6 @@
 import { Game } from "../types/game";
 import { Team } from "../types/team";
-import { espnGameURL, cbsGameURL } from "../types/baseURLs";
+import { espnCfbGameURL, cbsCfbGameURL } from "../types/baseURLs";
 
 
 type Props = {
@@ -11,9 +11,9 @@ type Props = {
 };
 
 const GameInfoModal = (props: Props) => {
-    const espnURL: string = `${espnGameURL}/${props.game.espnCode}`;
-    const cbsURL: string = `${cbsGameURL}/${props.game.cbsCode}`;
-    const modalID: string = `${props.game.gameID}-info`;
+    const espnCfbURL: string = `${espnCfbGameURL}/${props.game.espnCode}`;
+    const cbsCfbURL: string = `${cbsCfbGameURL}/${props.game.cbsCode}`;
+    const modalID: string = `${props.game.gameID}-info-modal`;
     const gameDate: Date = new Date(props.game.date);
     const gameYear = new Date(gameDate).getFullYear();
     const gameMonth = new Date(gameDate).getMonth();
@@ -25,33 +25,22 @@ const GameInfoModal = (props: Props) => {
     return (
         <div className="fixed flex h-[100vh] items-center justify-center left-0 top-0 w-[100vw] z-1000">
             <div className="bg-[#D9D9D9] p-10 relative text-black text-center" id={modalID}>
+                
                 <i className="fa-solid fa-rectangle-xmark absolute top-1 right-1" onClick={() => props.onClose()}></i>
                 <h1>{props.awayTeam.teamName} {props.awayTeam.teamMascot} @ {props.homeTeam.teamName} {props.homeTeam.teamMascot}</h1>
                 
-                <br />
-                <br />
+                <div>{localDate.toLocaleString().split(", ")[1]}</div>
 
-                <span>
-                    {/*{props.game.date.getMonth().toString()}/{props.game.date.getDay().toString()}/{props.game.date.getFullYear().toString()}<br /> */}
-                    {localDate.toLocaleString().split(", ")[1]}
-                    <br />
-                    {props.game.tvCoverage}<br />
-                    <br />
-                    {
-                        Number(props.game.espnAwayWinPercentage) > Number(props.game.espnHomeWinPercentage)
-                            &&
-                        <p>{props.game.espnAwayWinPercentage} {props.awayTeam.teamName}</p>
-                    }
-                    {
-                        Number(props.game.espnHomeWinPercentage) > Number(props.game.espnAwayWinPercentage)
-                            &&
-                        <p>{props.game.espnHomeWinPercentage} {props.homeTeam.teamName}</p>
-                    }
-                    <br />
+                <div>
+                    {props.game.tvCoverage.length > 1 && `Broadcast: ${props.game.tvCoverage}`}
+                </div>
+
+                <div>
                     <p>More Details:</p>
-                    <p className="text-[#34a8f8]"><a href={espnURL}>ESPN</a></p>
-                    <p className="text-[#34a8f8]"><a href={cbsURL}>CBS</a></p>
-                </span>
+                    <p className="text-[#34a8f8]"><a href={espnCfbURL}>ESPN</a></p>
+                    <p className="text-[#34a8f8]"><a href={cbsCfbURL}>CBS</a></p>
+                </div>
+
             </div>
         </div>
     )
--- src/types/baseURLs.ts
+++ src/types/baseURLs.ts
@@ -1,5 +1,5 @@
-export const espnGameURL: string = "https://www.espn.com/college-football/game/_/gameId";
-export const espnTeamURL: string = "https://www.espn.com/college-football/team/_/id";
+export const espnCfbGameURL: string = "https://www.espn.com/college-football/game/_/gameId";
+export const espnCfbTeamURL: string = "https://www.espn.com/college-football/team/_/id";
 
-export const cbsGameURL: string = "https://www.cbssports.com/college-football/scoreboard/FBS/2025/regular";
-export const cbsTeamURL: string = "https://www.cbssports.com/college-football/teams";
+export const cbsCfbGameURL: string = "https://www.cbssports.com/college-football/scoreboard/FBS/2025/regular";
+export const cbsCfbTeamURL: string = "https://www.cbssports.com/college-football/teams";

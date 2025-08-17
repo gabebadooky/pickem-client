2d20c22716ed8e648bd4856e4d3a7b1b02c6187a
Gabriel Baduqui <baduquig@gmail.com>
2025-08-17 12:21:56 -0700

    maintenance message

--- src/components/Maintenance.tsx
+++ src/components/Maintenance.tsx
@@ -1,7 +1,7 @@
 const Maintenance = () => {
     return(
         <div className="fixed h-[100vh] items-center justify-center m-auto mt-[10%] text-2xl w-[100vw] z-50">
-            <h1>Opps! We're resetting the first down markers! Please check back later.</h1>
+            <h1>We're resetting the first down markers! Please check back later.</h1>
             <br />
             <br />
             <img className="m-auto w-[35%]" src="/maintenance.png" alt="System under maintenance!" />

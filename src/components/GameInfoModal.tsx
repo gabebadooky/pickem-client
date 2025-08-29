f1eeb4a385380be49f9a7a0711004d1ea32c3983
Gabriel Baduqui <baduquig@gmail.com>
2025-08-28 17:42:10 -0700

    show score for games in progress

--- src/components/GameInfoModal.tsx
+++ src/components/GameInfoModal.tsx
@@ -43,7 +43,7 @@ const GameInfoModal = (props: Props) => {
                 <div className="text-xs">{localDate.toLocaleString().split(", ")[1]}</div>
 
                 <div className="mb-4 text-xs">
-                    {props.game.gameFinished == true && <b className="text-[#138e2e] text-xl">{props.game.awayTotalBoxScore} - {props.game.homeTotalBoxScore}</b>}
+                    {props.localKickoffTimestamp <= now && <b className="text-[#138e2e] text-xl">{props.game.awayTotalBoxScore} - {props.game.homeTotalBoxScore}</b>}
                     {props.game.tvCoverage.length > 1 && props.game.gameFinished == false && `Broadcast: ${props.game.tvCoverage}`}
                 </div>
 

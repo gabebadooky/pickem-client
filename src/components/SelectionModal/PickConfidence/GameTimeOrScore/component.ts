867e0c2f5a68fb62c7f0294a69c64ed791bdc5c4
Gabriel Baduqui <baduquig@gmail.com>
2025-10-02 20:48:05 -0700

    render game details if in progress

--- src/components/SelectionModal/PickConfidence/GameTimeOrScore/component.ts
+++ src/components/SelectionModal/PickConfidence/GameTimeOrScore/component.ts
@@ -5,7 +5,7 @@ import { ScoreboardEvent } from "../types";
 
 export const renderGameScore = async (game: Game): Promise<string> => {
     if (game.gameFinished) {
-        return `${game.awayTotalBoxScore} - ${game.homeTotalBoxScore}`;
+        return `Final: ${game.awayTotalBoxScore} - ${game.homeTotalBoxScore}`;
 
     } else {
         const gameEntryFromESPNHiddenAPI: ScoreboardEvent | undefined = await retrieveGameFromESPNHiddenAPI(game); //await retrieveGameFromESPNHiddenAPI(game);
@@ -14,16 +14,33 @@ export const renderGameScore = async (game: Game): Promise<string> => {
             return "0 - 0";
             
         } else {
+            const gameComplete: boolean = gameEntryFromESPNHiddenAPI.competitions[0].status.type.completed;
+
             if (gameEntryFromESPNHiddenAPI.competitions[0].competitors[0].homeAway === "away") {
                 const awayTeamScore: string = gameEntryFromESPNHiddenAPI.competitions[0].competitors[0].score;
                 const homeTeamScore: string = gameEntryFromESPNHiddenAPI.competitions[0].competitors[1].score;
 
-                return `${awayTeamScore} - ${homeTeamScore}`;
+                if (gameComplete) {
+                    return `Final: ${awayTeamScore} - ${homeTeamScore}`;
+
+                } else {
+                    const gameDetail: string = gameEntryFromESPNHiddenAPI.competitions[0].status.type.detail;
+                    return `${awayTeamScore} - ${homeTeamScore} | ${gameDetail}`;
+
+                }
             } else {
                 const awayTeamScore: string = gameEntryFromESPNHiddenAPI.competitions[0].competitors[1].score;
                 const homeTeamScore: string = gameEntryFromESPNHiddenAPI.competitions[0].competitors[0].score;
 
-                return `${awayTeamScore} - ${homeTeamScore}`;
+                if (gameComplete) {
+                    return `Final: ${awayTeamScore} - ${homeTeamScore}`;
+
+                } else {
+                    const gameDetail: string = gameEntryFromESPNHiddenAPI.competitions[0].status.type.detail;
+                    return `${awayTeamScore} - ${homeTeamScore} | ${gameDetail}`;
+
+                }
+
             }
         }
     }

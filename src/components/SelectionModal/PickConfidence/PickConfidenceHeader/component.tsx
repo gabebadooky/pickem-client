01bd156f541b156f3f937a6dc977d463749e5400
Gabriel Baduqui <baduquig@gmail.com>
2025-10-09 23:34:01 -0700

    fix team ranking bug

--- src/components/SelectionModal/PickConfidence/PickConfidenceHeader/component.tsx
+++ src/components/SelectionModal/PickConfidence/PickConfidenceHeader/component.tsx
@@ -16,11 +16,11 @@ const PickConfidenceHeader = (props: PickConfidenceHeaderProps) => {
     } else {
         return (
             <h1 className="h-full m-auto text-center w-full">
-                {props.homeTeam.teamName} {props.homeTeam.teamMascot} {props.awayTeam.ranking !== null ? `#${props.awayTeam.ranking}` : ""} ({`${formattedTeamRecord(props.homeTeam.overallWins, props.homeTeam.overallLosses, props.homeTeam.overallTies)}`})
+                {props.homeTeam.teamName} {props.homeTeam.teamMascot} {props.homeTeam.ranking !== null ? `#${props.homeTeam.ranking}` : ""} ({`${formattedTeamRecord(props.homeTeam.overallWins, props.homeTeam.overallLosses, props.homeTeam.overallTies)}`})
                 <br />
                 over
                 <br />
-                {props.awayTeam.teamName} {props.awayTeam.teamMascot} {props.homeTeam.ranking !== null ? `#${props.homeTeam.ranking}` : ""} ({`${formattedTeamRecord(props.awayTeam.overallWins, props.awayTeam.overallLosses, props.awayTeam.overallTies)}`})
+                {props.awayTeam.teamName} {props.awayTeam.teamMascot} {props.awayTeam.ranking !== null ? `#${props.awayTeam.ranking}` : ""} ({`${formattedTeamRecord(props.awayTeam.overallWins, props.awayTeam.overallLosses, props.awayTeam.overallTies)}`})
             </h1>
         );
     }

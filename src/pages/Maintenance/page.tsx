3c2b320e2856e7b546fceef001dbcba046cdf08a
Gabriel Baduqui <baduquig@gmail.com>
2025-12-01 10:27:37 -0700

    Maintenance

---
+++ src/pages/Maintenance/page.tsx
@@ -0,0 +1,12 @@
+const Maintenance = () => {
+    return(
+        <div className="fixed h-[100vh] items-center justify-center m-auto mt-[10%] text-2xl w-[100vw] z-50">
+            <h1>Opps! We're resetting the first down markers! Please check back later.</h1>
+            <br />
+            <br />
+            <img className="m-auto w-[35%]" src="/maintenance.png" alt="System under maintenance!" />
+        </div>
+    )
+}
+
+export default Maintenance;
